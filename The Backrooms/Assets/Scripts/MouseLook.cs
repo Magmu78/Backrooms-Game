d2a74b6e1de8169b2680315dd5f69391f2589866
Magmu78 <magmu78@gmail.com>
2022-11-15 18:28:17 +0100

    Add Backrooms Folder

---
+++ The Backrooms/Assets/Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity;
+    public Transform playerBody;
+
+    float xRot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        xRot -= mouseY * mouseSensitivity;
+        xRot = Mathf.Clamp(xRot, -80, 80);
+
+        transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        playerBody.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * mouseSensitivity, 0);
+    }
+}
---
+++ The Backrooms/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+    public float gravity = 30f;
+    public float groundDistance = 0.4f;
+    public bool isGrounded;
+
+    public Transform groundCheck;
+
+    CharacterController characterController;
+
+    Vector3 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        Vector3 movement = transform.forward * vertical + transform.right * horizontal;
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(movement * speed * Time.deltaTime);
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
