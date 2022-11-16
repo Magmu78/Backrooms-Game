aac3c357cd155adc80cea35157165e897ab65e10
Magmu78 <magmu78@gmail.com>
2022-11-16 17:18:24 +0100

    Fixed Lights And Added Footsteps
    A huge Update

--- The Backrooms/Assets/Scripts/PlayerMovement.cs
+++ The Backrooms/Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,21 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 6f;
     public float gravity = 30f;
     public float groundDistance = 0.4f;
+    float vertical;
+    float horizontal;
     public bool isGrounded;
 
     public Transform groundCheck;
 
+    [Header("Footsteps Parameters")]
+    public AudioSource footAudioSource;
+    public AudioClip[] footsteps;
+    //public float minDistance = 0.2f;
+    private float footstepsTimer = 0;
+
     CharacterController characterController;
+
+    Camera playerCamera;
 
     Vector3 velocity;
 
@@ -19,6 +29,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        playerCamera = GetComponentInChildren<Camera>();
     }
 
     // Update is called once per frame
@@ -26,8 +37,8 @@ public class PlayerMovement : MonoBehaviour
     {
         float deltaTime = Time.deltaTime;
 
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
 
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance);
 
@@ -41,7 +52,23 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         characterController.Move(movement * speed * Time.deltaTime);
+        HandleFootsteps();
 
         characterController.Move(velocity);
+    }
+
+    public void HandleFootsteps()
+    {
+        if (!isGrounded || vertical == 0 && horizontal == 0) return;
+        footstepsTimer -= Time.deltaTime;
+
+        if(footstepsTimer <= 0)
+        {
+            if(Physics.Raycast(playerCamera.transform.position, Vector3.down, out RaycastHit hit, 2.5f))
+            {
+                footAudioSource.PlayOneShot(footsteps[Random.Range(0, footsteps.Length - 1)]);
+            }
+            footstepsTimer = 0.5f;
+        }
     }
 }
